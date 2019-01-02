62138d0576fcd854cbe2d28d5e4d3408b50087a3
Daniel Phiri <malgamves@gmail.com>
2019-01-02 18:20:58 +0530

    Add files via upload

---
+++ WebApplication1/Controllers/ValuesController.fs
@@ -0,0 +1,30 @@
+﻿namespace WebApplication1.Controllers
+
+open System.Web
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Twilio
+open Twilio.Rest.Api.V2010.Account
+open Twilio.Types
+open Twilio.AspNet.Common;
+open Twilio.AspNet.Mvc;
+open Twilio.TwiML;
+open Microsoft.AspNetCore.Mvc
+
+[<Route("[controller]")>]
+[<ApiController>]
+type ValuesController () =
+    inherit TwilioController()
+
+
+    [<HttpGet>]
+    member this.Get() =
+        let values = [|"value1"; "value2"|]
+        ActionResult<string[]>(values)
+
+    [<HttpPost>]
+    member this.Index (request : SmsRequest) =
+        let response = MessagingResponse()
+        response.Message "Hello World" |> ignore
+        this.TwiML response
